63f2655ba808b5a3bfbbce36f78955621a493404
xxelementsxx <113716944+xxelementsxx@users.noreply.github.com>
2022-09-18 18:16:12 +0200

    3commit

--- data_vis_with_flowsom_R.R
+++ data_vis_with_flowsom_R.R
@@ -1,9 +1,13 @@
+#elder version of R
+
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 pkgs <- rownames(installed.packages())
 BiocManager::install(pkgs, type = "source", checkBuilt = TRUE)
 
 source("https://bioconductor.org/biocLite.R")
+
+#new version start here:
 
 #https://bioconductor.org/install/index.html#find-bioconductor-packages
 #https://bioconductor.org/install/
@@ -31,4 +35,17 @@ if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
 BiocManager::install("flowCore")
+
+BiocManager::install("FlowSOM")
+install.packages("pheatmap")
+
+#download: https://www.flowjo.com/solutions/flowjo/downloads
+#download: https://www.flowjo.com/solutions/seqgeq/downloads
+
+#download flowJo plugIns: https://www.flowjo.com/exchange/#/
+#unzip & save plugIns under: C:\Program Files\FlowJo 10.8.1\plugins
+#register on flowJo: https://www.flowjo.com/solutions/flowjo/free-trial
+#copy serial to flowJo
+
+read.csv("C:/Users/Melissa/Desktop/BP-DAV/data visualization/Dataset/covid.csv")
 
